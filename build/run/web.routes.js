b1653d0ee0b1c42f1666a68de75e56264d1d1cdf
R.Phuwanat <r.phuwanat.dev@gmail.com>
2021-11-17 08:13:40 +0700

    Ignore build

--- build/run/web.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var router_1 = __importDefault(require("../app/kernel/router"));
-router_1.default.get("/", "home@index", false);
-router_1.default.get("/hello", "home@hello", false);
