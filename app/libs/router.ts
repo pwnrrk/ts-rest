255761924b8add6104fae5aedaa3dd9677db09c6
R.Phuwanat <phuwanat.r@ttcl.com>
2022-05-27 17:30:23 +0700

    update route

--- app/libs/router.ts
+++ app/libs/router.ts
@@ -12,7 +12,7 @@ function applyFuntion(
   routePath: string,
   method: string,
   functionPath: string,
-  authenticate: boolean
+  authenticate?: boolean
 ) {
   const splitted = functionPath.split("@");
   const classPath = splitted[0];
@@ -22,22 +22,25 @@ function applyFuntion(
     routePath,
     classPath,
     functionName,
-    authenticate,
+    authenticate: authenticate === true,
   };
   Routes.push(route);
 }
 
 export default class Router {
-  static get(path: string, functionPath: string, authenticate: boolean) {
+  static get(path: string, functionPath: string, authenticate?: boolean) {
     applyFuntion(path, "get", functionPath, authenticate);
   }
-  static post(path: string, functionPath: string, authenticate: boolean) {
+  static post(path: string, functionPath: string, authenticate?: boolean) {
     applyFuntion(path, "post", functionPath, authenticate);
   }
-  static put(path: string, functionPath: string, authenticate: boolean) {
+  static put(path: string, functionPath: string, authenticate?: boolean) {
     applyFuntion(path, "put", functionPath, authenticate);
   }
-  static delete(path: string, functionPath: string, authenticate: boolean) {
+  static delete(path: string, functionPath: string, authenticate?: boolean) {
+    applyFuntion(path, "delete", functionPath, authenticate);
+  }
+  static patch(path: string, functionPath: string, authenticate?: boolean) {
     applyFuntion(path, "delete", functionPath, authenticate);
   }
 }
